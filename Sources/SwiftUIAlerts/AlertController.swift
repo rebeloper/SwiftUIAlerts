f804a3aaaf5ccc39411b27a0295121652cc17312
Alex Nagy <rebeloperalpar@gmail.com>
2024-08-08 12:18:21 +0300

    alert title and message requerment

--- Sources/SwiftUIAlerts/AlertController.swift
+++ Sources/SwiftUIAlerts/AlertController.swift
@@ -19,6 +19,9 @@ public class AlertController {
     ///   - message: Message, defaults to empty String
     ///   - buttons: Buttons, defaults to on OK button
     public func present(_ type: AlertType = .alert, title: String = "", message: String = "", buttons: [AlertButton] = [AlertButton(.ok)]) {
+        if type == .alert, title == "", message == "" {
+            fatalError("Alerts should have at least a title or a message")
+        }
         self.alertDetails = AlertDetails(type: type, title: title, message: message, buttons: buttons)
     }
     
